ba3fb11e887214b74d5c90461f9800d45fe7455d
Heider Arellano Oviedo <90737937+HeyderAndres@users.noreply.github.com>
2026-02-11 17:57:06 -0500

    [Delete] files second course

--- src/04-never.ts
+++
@@ -1,8 +0,0 @@
-// never es un tipo de dato que representa un valor que nunca ocurre, es decir que no puede ser asignado a ninguna variable, ni ser retornado por ninguna función.
-// el tipo never se utiliza principalmente para indicar que una función no retorna ningun valor, ya sea porque lanza una excepcion o porque entra en un bucle infinito.
-
-function throwError(message: string) {
-  throw new Error(message);
-};
-
-throwError('Error');
--- src/app/orders/order.model.ts
+++
@@ -1,9 +0,0 @@
-import { BaseModel } from "../base.model";
-import { Product } from "../products/product.model";
-import { User } from "../users/user.model";
-
-interface Order extends BaseModel{
-  createdAt: Date;
-  products: Product[];
-  user: User;
-}
